b980cf15b98768caef8ceeb166f61710d72a55f1
DongyuZhang <dongyuzhang@gwu.edu>
2018-06-09 16:02:50 -0400

    Solved a Hard Problem with Explaination

--- BinaryTreeMaxPathSum/src/Solution.java
+++ BinaryTreeMaxPathSum/src/Solution.java
@@ -1,15 +1,30 @@
 public class Solution {
     public static void main(String[] args){}
 
+    private int max_sum = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-
+        int max_with_root = maxPathDown(root);
+        return max_sum;
     }
-    private int getMaxPathSumWithRoot(TreeNode root){
-        if(root == null)
+
+    /**
+     * Use a global variable to calculate max path sum each time we get the
+     * left and right maxPathDown
+     * @param root
+     * @return
+     * The maximum sum of the path from this root down to the leaf of that path
+     * This sum can be used by the parent of this root to calculate parent's
+     * maxPathDown.
+     */
+    private int maxPathDown(TreeNode root){
+        if (root == null) {
             return 0;
-        int leftSum = Math.max(getMaxPathSumWithRoot(root.left), 0);
-        int rightSum = Math.max(getMaxPathSumWithRoot(root.right), 0);
-        int res = Math.max()
+        }
+        int left_max = Math.max(maxPathDown(root.left), 0);
+        int right_max = Math.max(maxPathDown(root.right), 0);
+        int res = left_max+root.val+right_max;
+        max_sum = Math.max(res, max_sum);
+        return Math.max(left_max, right_max)+root.val;
     }
 }
 
